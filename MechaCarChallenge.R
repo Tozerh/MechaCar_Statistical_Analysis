24495d8f5f7385bac9e49adde182e5b6289a83f9
Tozerh <tozer.hammond@gmail.com>
2022-02-17 19:27:10 -0500

    Upload for Deliv2 and Screenshots

---
+++ MechaCarChallenge.R
@@ -0,0 +1,5 @@
+library(dplyr)
+mechaData <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+head(mechaData)
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechaData)
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechaData))
